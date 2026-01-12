415ee9778410a24575c4a5882839ed1b5e5b6157
davidaltafini-collab <david.altafini@gmail.com>
2026-01-12 18:34:31 +0200

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import Stripe from 'stripe';
-import { pool, checkDbConnection } from './db'; 
+import { pool, checkDbConnection } from './db.js'; 
 import { sendOrderEmails } from './services/email';
 
 dotenv.config();
