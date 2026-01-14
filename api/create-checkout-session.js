86beff678bc57f209bcafbe3dedcbd0b839578bb
davidaltafini-collab <david.altafini@gmail.com>
2026-01-14 15:58:14 +0200

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -44,6 +44,7 @@ export default async function handler(req, res) {
       payment_method_types: ['card'],
       line_items: lineItems,
       mode: 'payment',
+      locale: 'ro', // ← FIX PENTRU "Cannot find module './en'"
       billing_address_collection: 'required',
       shipping_address_collection: {
         allowed_countries: ['RO'],
