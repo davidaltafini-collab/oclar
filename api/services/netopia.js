8397c920801c18ae32f45007eb1c8ee1ca97a122
davidaltafini-collab <david.altafini@gmail.com>
2026-02-06 17:00:14 +0200

    a

--- api/services/netopia.js
+++ api/services/netopia.js
@@ -3,41 +3,70 @@ import fs from 'fs';
 import path from 'path';
 
 const NETOPIA_CONFIG = {
-  // ⭐ SCHIMBAT PENTRU LIVE (Productie)
-  url: 'http://sandboxsecure.mobilpay.ro', 
-  // Semnatura se ia din .env, sau poti pune string-ul direct aici daca vrei
+  url: 'http://sandboxsecure.mobilpay.ro', // Sandbox
   signature: process.env.NETOPIA_SIGNATURE, 
   publicKeyPath: path.join(process.cwd(), 'api', 'certs', 'public.cer'),
   privateKeyPath: path.join(process.cwd(), 'api', 'certs', 'private.key')
 };
 
-// --- 1. INITIERE PLATA (CRIPTARE) ---
+// --- ALGORITM RC4 PUR (JavaScript) ---
+// Îl scriem manual ca să nu depindem de mofturile Node.js
+function rc4(key, str) {
+	var s = [], j = 0, x, res = '';
+	for (var i = 0; i < 256; i++) { s[i] = i; }
+	for (i = 0; i < 256; i++) {
+		j = (j + s[i] + key.charCodeAt(i % key.length)) % 256;
+		x = s[i]; s[i] = s[j]; s[j] = x;
+	}
+	i = 0; j = 0;
+	for (var y = 0; y < str.length; y++) {
+		i = (i + 1) % 256;
+		j = (j + s[i]) % 256;
+		x = s[i]; s[i] = s[j]; s[j] = x;
+		res += String.fromCharCode(str.charCodeAt(y) ^ s[(s[i] + s[j]) % 256]);
+	}
+	return res;
+}
+
+function stringToHex(str) {
+    let hex = '';
+    for(let i=0;i<str.length;i++) {
+        hex += ''+str.charCodeAt(i).toString(16).padStart(2, '0');
+    }
+    return hex.toUpperCase();
+}
+
+function hexToString(hex) {
+    let str = '';
+    for (let i = 0; i < hex.length; i += 2) {
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    }
+    return str;
+}
+
+// --- 1. INITIERE PLATA ---
 export const encryptRequest = async (paymentData) => {
   try {
+    // Citim certificatul
     const publicKeyPem = fs.readFileSync(NETOPIA_CONFIG.publicKeyPath, 'utf8');
-    
-    // ⭐ AICI ESTE FIX-UL PENTRU EROAREA "PEM header type"
     let publicKey;
     try {
-        // Încercăm să citim ca un certificat (formatul standard Netopia)
         const cert = forge.pki.certificateFromPem(publicKeyPem);
         publicKey = cert.publicKey;
     } catch (e) {
-        // Fallback: Dacă e formatul vechi (doar cheie publică)
         publicKey = forge.pki.publicKeyFromPem(publicKeyPem);
     }
 
     const xmlData = buildXml(paymentData);
 
-    // Criptare RC4 (Datele)
+    // Generam cheia RC4 aleatorie
     const rc4Key = forge.random.getBytesSync(16);
-    const cipher = forge.cipher.createCipher('RC4', rc4Key);
-    cipher.start();
-    cipher.update(forge.util.createBuffer(xmlData, 'utf8'));
-    cipher.finish();
-    const encryptedData = cipher.output.toHex().toUpperCase();
+    
+    // CRIPTARE CU FUNCTIA NOASTRA MANUALĂ (Nu dă erori de sistem)
+    const encryptedRaw = rc4(rc4Key, xmlData);
+    const encryptedData = stringToHex(encryptedRaw);
 
-    // Criptare RSA (Cheia RC4)
+    // Criptam cheia RC4 cu RSA (Asta merge bine cu Forge)
     const encryptedKey = publicKey.encrypt(rc4Key, 'RSA-OAEP');
     const envKey = forge.util.encode64(encryptedKey);
 
@@ -52,24 +81,19 @@ export const encryptRequest = async (paymentData) => {
   }
 };
 
-// --- 2. DECRIPTARE RĂSPUNS (IPN) ---
+// --- 2. DECRIPTARE RĂSPUNS ---
 export const decryptIPN = async (envKey, encryptedData) => {
     try {
         const privateKeyPem = fs.readFileSync(NETOPIA_CONFIG.privateKeyPath, 'utf8');
         const privateKey = forge.pki.privateKeyFromPem(privateKeyPem);
 
-        // Decriptăm cheia RC4 folosind RSA
+        // Decriptăm cheia RC4
         const rc4Key = privateKey.decrypt(forge.util.decode64(envKey), 'RSA-OAEP');
 
-        // Decriptăm datele folosind RC4
-        const decipher = forge.cipher.createDecipher('RC4', rc4Key);
-        decipher.start();
-        decipher.update(forge.util.createBuffer(forge.util.hexToBytes(encryptedData)));
-        decipher.finish();
-        
-        const xmlContent = decipher.output.toString();
+        // Decriptăm datele cu funcția manuală
+        const encryptedStr = hexToString(encryptedData);
+        const xmlContent = rc4(rc4Key, encryptedStr);
 
-        // Extragem datele simplu (fără parser XML greoi)
         const orderIdMatch = xmlContent.match(/id="([^"]+)"/);
         const actionMatch = xmlContent.match(/<action>([^<]+)<\/action>/);
         const errorMatch = xmlContent.match(/<error code="([^"]+)">/);
@@ -89,12 +113,8 @@ export const decryptIPN = async (envKey, encryptedData) => {
 
 // --- 3. CONSTRUIRE XML ---
 function buildXml(data) {
-    const today = new Date();
-    const startDate = today.toISOString().split('T')[0] + ' ' + today.toTimeString().split(' ')[0];
-    // Data de expirare (ex: +10 minute)
-    const expireDateObj = new Date(today.getTime() + 10 * 60000); 
-    const expireDate = expireDateObj.toISOString().split('T')[0] + ' ' + expireDateObj.toTimeString().split(' ')[0];
-
+    const clean = (str) => String(str || '').replace(/[<>&'"]/g, '');
+    
     return `<?xml version="1.0" encoding="utf-8"?>
 <order type="card" timestamp="${Date.now()}">
   <signature>${NETOPIA_CONFIG.signature}</signature>
@@ -106,18 +126,18 @@ function buildXml(data) {
     <details>Comanda Oclar #${data.orderId}</details>
     <contact_info>
       <billing type="person">
-        <first_name>${data.firstName}</first_name>
-        <last_name>${data.lastName}</last_name>
-        <email>${data.email}</email>
-        <address>${data.address}</address>
-        <mobile_phone>${data.phone}</mobile_phone>
+        <first_name>${clean(data.firstName)}</first_name>
+        <last_name>${clean(data.lastName)}</last_name>
+        <email>${clean(data.email)}</email>
+        <address>${clean(data.address)}</address>
+        <mobile_phone>${clean(data.phone)}</mobile_phone>
       </billing>
       <shipping type="person">
-        <first_name>${data.firstName}</first_name>
-        <last_name>${data.lastName}</last_name>
-        <email>${data.email}</email>
-        <address>${data.address}</address>
-        <mobile_phone>${data.phone}</mobile_phone>
+        <first_name>${clean(data.firstName)}</first_name>
+        <last_name>${clean(data.lastName)}</last_name>
+        <email>${clean(data.email)}</email>
+        <address>${clean(data.address)}</address>
+        <mobile_phone>${clean(data.phone)}</mobile_phone>
       </shipping>
     </contact_info>
   </invoice>
